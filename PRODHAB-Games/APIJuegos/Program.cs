521f48861f8837a1a53e18c2ce3a716900fef9f1
Albin Liang <130929541+AlbinJunLiang@users.noreply.github.com>
2025-08-25 05:37:28 -0600

    Update Program.cs

--- PRODHAB-Games/APIJuegos/Program.cs
+++ PRODHAB-Games/APIJuegos/Program.cs
@@ -4,18 +4,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-<<<<<<< HEAD
-=======
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
->>>>>>> fc197293d377b5c4aff6d6fc7e645c1f70482542
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<PracticaJuegosUcrContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-<<<<<<< HEAD
 // 🔹 Permitir CORS desde cualquier origen
 builder.Services.AddCors(options =>
 {
@@ -28,15 +25,13 @@ builder.Services.AddCors(options =>
     });
 });
 
-=======
->>>>>>> fc197293d377b5c4aff6d6fc7e645c1f70482542
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-<<<<<<< HEAD
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API v1");
@@ -48,7 +43,6 @@ app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 app.MapControllers();
-=======
    app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mi API v1");
@@ -59,6 +53,5 @@ app.MapControllers();
 app.UseHttpsRedirection();
 app.MapControllers(); // esto es importante
 
->>>>>>> fc197293d377b5c4aff6d6fc7e645c1f70482542
 
 app.Run();
